a36afd39e0af55a8cb9fc7daf5d833f6b197f40e
Miguel Sanz <sanzmartinmiguel@gmail.com>
2018-11-09 18:31:27 +0100

    First observable

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-alert('Hello RxJS!');
+import { Observable } from 'rxjs';
+import { allBooks } from './data';
+
+let allBooksObservable$ = new Observable(subscriber => {
+  
+  if (document.title !== 'RxBookTracker') {
+    subscriber.error('Incorrect page title');
+  }
+
+  for (let book of allBooks) {
+    subscriber.next(book);
+  }
+
+  setTimeout(() => {
+    subscriber.complete();
+  }, 2000);
+
+  return () => console.log('Executing teardown code');
+
+});
+
+allBooksObservable$.subscribe((book: any) => console.log(book.title));
